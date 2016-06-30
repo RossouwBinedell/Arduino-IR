135665adc5f47056822c3c0989cc957c93349916
Rossouw Binedell <ross.binedell@gmail.com>
2016-06-30 13:50:58 +0200

    Inital Comment

---
+++ ArduionoIR/app/build/generated/source/buildConfig/androidTest/debug/maintest/arduionoir/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package maintest.arduionoir.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "maintest.arduionoir.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
